85ada04a6944b5bc4287e0589769351888fa0c48
King Chung Huang <kinghuang@mac.com>
2017-04-25 16:27:10 -0600

    Create invoke_for decorator to register invokers
    The invoke_for decorator registers a function to be called when a
    service is discovered whose finvoke label matches the specified pattern.

---
+++ finvoker/invoker.py
@@ -0,0 +1,12 @@
+import re
+
+
+invokers = []
+
+
+def invoke_for(matchstr, flags=0):
+    def wrapper(func):
+        invokers.add((re.compile(matchstr, flags), func))
+        logger.info(f'Registered {func.__name__} for {matchstr}')
+        return func
+    return wrapper
