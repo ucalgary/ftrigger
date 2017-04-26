72fa72049b7cf8adca58c09fa5ff1e9a8240d5a5
King Chung Huang <kinghuang@mac.com>
2017-04-25 19:32:37 -0600

    Notify invokers based on service changes
    Notify invokers for new, updated, or removed services.

--- finvoker/invoker.py
+++ finvoker/invoker.py
@@ -39,15 +39,18 @@ class InvocationManager(object):
             if not existing_service:
                 # register a new service
                 log.debug(f'New service: {service.attrs["Spec"]["Name"]} ({service.id})')
+                self._notify_for_service(service, 1)
                 self._services[service.id] = service
             elif service.attrs['UpdatedAt'] > existing_service.attrs['UpdatedAt']:
                 # maybe update an already registered service
                 log.debug(f'Updated service: {service.attrs["Spec"]["Name"]} ({service.id})')
+                self._notify_for_service(service, 2)
 
         # Scan for removed services
         for service_id in set(self._services.keys()) - set([s.id for s in services]):
             service = self._services.pop(service_id)
             log.debug(f'Removed service: {service.attrs["Spec"]["Name"]} ({service.id})')
+            self._notify_for_service(service, 3)
 
         self.last_refresh = datetime.datetime.utcnow().isoformat()
         self.loop.call_later(self.refresh_interval, self.refresh_services)
