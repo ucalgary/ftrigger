eaa47135d912e6f0ef2aab1b89212558ec9ac6c6
King Chung Huang <kinghuang@mac.com>
2017-04-25 18:19:40 -0600

    Incorporate invoker registration into InvocationManager
    Turn the invoker_for decorator into a register method in
    InvocationManager. Decorator-based registration will not be used.

--- finvoker/invoker.py
+++ finvoker/invoker.py
@@ -4,12 +4,27 @@ import re
 
 import docker
 
-invokers = []
 
+class InvocationManager(object):
 
-def invoke_for(matchstr, flags=0):
-    def wrapper(func):
-        invokers.add((re.compile(matchstr, flags), func))
+    def __init__(self, refresh_interval=5):
+        self.client = docker.from_env()
+        self.loop = asyncio.new_event_loop()
+        self.refresh_interval = refresh_interval
+        self.last_refresh = ''
+        self._invokers = []
+
+    def register(self, matchstr, add_f, remove_f=None):
+        invokers.add((re.compile(matchstr, flags), add_f, remove_f))
         logger.info(f'Registered {func.__name__} for {matchstr}')
-        return func
-    return wrapper
+
+    def run(self):
+        self.refresh_services()
+        self.loop.run_forever()
+
+    def refresh_services(self):
+        services = filter(lambda s: s.attrs['UpdatedAt'] > self.last_refresh,
+                          self.client.services.list())
+        print([s.attrs['Spec']['Name'] for s in services])
+        self.last_refresh = datetime.datetime.utcnow().isoformat()
+        self.loop.call_later(self.refresh_interval, self.refresh_services)
