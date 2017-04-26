75329260ee786bb6df78ecb9f679fd3c33a44bf3
King Chung Huang <kinghuang@mac.com>
2017-04-25 19:02:50 -0600

    Scan for new, updated, and removed services
    Scan the services list for candidate services, and determine if they
    are new, updated, or removed. Candidate services are services that have
    a finvoker label.

--- finvoker/invoker.py
+++ finvoker/invoker.py
@@ -17,6 +17,7 @@ class InvocationManager(object):
         self.refresh_interval = refresh_interval
         self.last_refresh = ''
         self._invokers = []
+        self._services = {}
 
     def register(self, matchstr, add_f, remove_f=None):
         invokers.add((re.compile(matchstr, flags), add_f, remove_f))
@@ -27,8 +28,26 @@ class InvocationManager(object):
         self.loop.run_forever()
 
     def refresh_services(self):
-        services = filter(lambda s: s.attrs['UpdatedAt'] > self.last_refresh,
-                          self.client.services.list())
-        print([s.attrs['Spec']['Name'] for s in services])
+        services = list(filter(lambda s: 'finvoker' in s.attrs.get('Spec', {}).get('Labels', {}),
+                               self.client.services.list()))
+
+        # Scan for new and updated services
+        for service in services:
+            existing_service = self._services.get(service.id)
+
+            if not existing_service:
+                # register a new service
+                log.debug(f'New service: {service.attrs["Spec"]["Name"]} ({service.id})')
+            elif service.attrs['UpdatedAt'] > existing_service.attrs['UpdatedAt']:
+                # maybe update an already registered service
+                log.debug(f'Updated service: {service.attrs["Spec"]["Name"]} ({service.id})')
+
+            self._services[service.id] = service
+
+        # Scan for removed services
+        for service_id in set(self._services.keys()) - set([s.id for s in services]):
+            service = self._services.pop(service_id)
+            log.debug(f'Removed service: {service.attrs["Spec"]["Name"]} ({service.id})')
+
         self.last_refresh = datetime.datetime.utcnow().isoformat()
         self.loop.call_later(self.refresh_interval, self.refresh_services)
