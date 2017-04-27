3ff297f765416f8451b26603ae4f5c6959f5056d
King Chung Huang <kinghuang@mac.com>
2017-04-27 16:20:35 -0600

    Parse arguments for specific invoker type
    Extract the arguments for specific invoker types by matching on labels
    with the key pattern finvoker.<type>.<arg>.

--- finvoker/invoker.py
+++ finvoker/invoker.py
@@ -58,15 +58,17 @@ class InvocationManager(object):
 
     def _notify_for_service(self, service, function_idx):
         labels = service.attrs.get('Spec', {}).get('Labels', {})
-        invoker_types = [m.group(1) for m in [self._type_pattern.match(k) for k in labels.keys()] if m]
+        invoker_types = [m.group(1) for m
+                         in [self._type_pattern.match(k) for k in labels.keys()] if m]
 
         for invoker_type in invoker_types:
             matching_invokers = list(filter(lambda i: i[0].match(invoker_type), self.invokers))
             if not matching_invokers:
                 continue
 
-            invoker_args = {k[len(self._invoker_label) + 1:]: v for k, v in labels.items()
-                            if k.startswith(self._invoker_label + '_')}
+            invoker_arg_pattern = re.compile(f'^{self._invoker_label}\\.{invoker_type}\\.([^.]+)$')
+            invoker_args = {m.group(1): v for m, v
+                            in [(invoker_arg_pattern.match(k), v) for k, v in labels.items()] if m}
             log.debug(f'Invoker arguments: {invoker_args}')
             [i[function_idx](service, **finvoker_args) for i in matching_invokers if i[function_idx]]
 
