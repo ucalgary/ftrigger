3d7b5ea582c1af29f05e387d3c1002da83d2043b
King Chung Huang <kinghuang@mac.com>
2017-05-02 09:51:13 -0600

    Create a simple invoker based on Kafka messages
    KafkaInvoker subscribes to topics specified by function services and
    calls the function when a message is received on the topic.

---
+++ finvoker/kafka.py
@@ -0,0 +1,72 @@
+import atexit
+import collections
+import logging
+
+import requests
+try:
+    import ujson as json
+except:
+    import json
+from confluent_kafka import Consumer
+
+from .invoker import InvokerBase
+
+
+log = logging.getLogger(__name__)
+
+
+class KafkaInvoker(InvokerBase):
+
+    def __init__(self, label='finvoker', name='kafka', refresh_interval=5,
+                 kafka=('kafka:9092',)):
+        super().__init__(label=label, name=name, refresh_interval=refresh_interval)
+        self.config = {
+            'bootstrap.servers': ','.join(kafka),
+            'group.id': self._register_label,
+            'default.topic.config': {
+                'auto.offset.reset': 'largest',
+                'auto.commit.interval.ms': 5000
+            }
+        }
+
+    def run(self):
+        consumer = Consumer(self.config)
+        callbacks = collections.defaultdict(list)
+
+        def close():
+            log.info('Closing consumer')
+            consumer.close()
+        atexit.register(close)
+
+        while True:
+            add, update, remove = self.refresh_services()
+            if add or update or remove:
+                existing_topics = set(callbacks.keys())
+
+                for s in add:
+                    callbacks[self.arguments(s).get('topic')].append(s)
+                for s in update:
+                    pass
+                for s in remove:
+                    callbacks[self.arguments(s).get('topic')].remove(s)
+
+                interested_topics = set(callbacks.keys())
+
+                if existing_topics.symmetric_difference(interested_topics):
+                    log.debug(f'Subscribing to {interested_topics}')
+                    consumer.subscribe(list(interested_topics))
+
+            message = consumer.poll(timeout=self.refresh_interval)
+            if not message:
+                log.debug('Empty message received')
+            elif not message.error():
+                topic, key, value = message.topic(), \
+                                    message.key().decode('utf-8'), \
+                                    json.loads(message.value())
+                for service in callbacks[topic]:
+                    requests.post(f'http://gateway:8080/function/{service.attrs["Spec"]["Name"]}', data=key)
+
+
+def main():
+    invoker = KafkaInvoker()
+    invoker.run()
