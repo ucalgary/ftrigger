d8d217a51e9b44008b3d4a4b7e9cb042f67cc6c9
King Chung Huang <kinghuang@mac.com>
2017-09-11 13:23:35 -0600

    Filter for functions with trigger label
    Filter the functions from the gateway for those whose service contain
    the trigger label. This is done based on the presence of
    _register_label in the service's list of labels.

--- ftrigger/trigger.py
+++ ftrigger/trigger.py
@@ -81,6 +81,8 @@ class TriggerBase(object):
         functions = self.gateway.get(self._gateway_base + '/system/functions').json()
         for function in functions:
             function['service'] = self.client.services.get(function['name'])
+        functions = list(filter(lambda f: self._register_label in f['service'].attrs.get('Spec', {}).get('Labels', {}),
+                                functions))
 
         # Scan for new and updated functions
         for function in functions:
